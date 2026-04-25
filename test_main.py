ddba82530dc6b4f09fbee7071931afdb11ca5bfb
veto2922 <veto2922@gmail.com>
2026-04-25 17:13:30 +0300

    langgraph block

--- test_main.py
+++ test_main.py
@@ -9,28 +9,62 @@
 ##########################################################
 
 
-from src.retrieval_block.tree_retrieval import TreeRetrieval
+# from src.retrieval_block.tree_retrieval import TreeRetrieval
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# from pageindex import PageIndexClient
+# from loguru import logger
+
+# load_dotenv()
+
+# Page_index_api = os.getenv("Page_index_api")
+
+# pi_client = PageIndexClient(api_key=Page_index_api)
+
+# tr = TreeRetrieval(pi_client)
+
+# target_docs = [{"doc_index": 0, "target_ids": ["0007"]}]
+
+# print(tr.get_docs_titles())
+
+# print(" * " * 50)
+
+# print(tr.get_toc([0, 1]))
+
+# print(" * " * 50)
+# print(tr.retrieve(target_docs))
+
+
+#####################################################################
+from src.graph_block.graph import compile_graph
+from src.retrieval_block.tree_retrieval import TreeRetrieval
 from pageindex import PageIndexClient
-from loguru import logger
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-Page_index_api = os.getenv("Page_index_api")
+api_key = os.getenv("Page_index_api")
 
-pi_client = PageIndexClient(api_key=Page_index_api)
+if not api_key:
+    raise ValueError("PageIndex API key is missing.")
 
-tr = TreeRetrieval(pi_client)
+pi_client = PageIndexClient(api_key=api_key)
 
-target_docs = [{"doc_index": 0, "target_ids": ["0007"]}]
 
-print(tr.get_docs_titles())
+tree_retrieval = TreeRetrieval(pi_client)
 
-print(" * " * 50)
+agent = compile_graph(tree_retrieval)
 
-print(tr.get_toc([0, 1]))
+docs_titles = tree_retrieval.get_docs_titles()
 
-print(" * " * 50)
-print(tr.retrieve(target_docs))
+config = {"configurable": {"thread_id": "1"}}
+
+res = agent.invoke(
+    {"user_query": "ايه هي المشاريع الي اشتغلت عليها؟", "docs_titles": docs_titles},
+    config,
+)
+
+for m in res["messages"]:
+    m.pretty_print()
